b0d0f22124756813e3cb76aeae1771d9d5b69a09
ontofractal <ontofractal@users.noreply.github.com>
2018-12-29 16:23:28 +0200

    add vim-like shortcuts for worskpace navigation

---
+++ shortcuts.bash
@@ -0,0 +1,9 @@
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-left "['<Ctrl><Alt>h']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-right "['<Ctrl><Alt>l']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-up "['<Ctrl><Alt>k']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-down "['<Ctrl><Alt>j']"
+ 
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-left "['<Ctrl><Alt><Shift>h']"
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-right "['<Ctrl><Alt><Shift>l']"
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-up "['<Ctrl><Alt><Shift>k']"
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-down "['<Ctrl><Alt><Shift>j']"
